04e56d875a3ecd43c9cff5dbe5ad7422ae5825ab
KarishmaP05 <phapalekarishma80@gmail.com>
2024-09-20 13:51:46 +0530

    all changes

--- InsurancePolicy/views.py
+++ InsurancePolicy/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.core.mail import send_mail
 from .models import Customer,Lead,Policy,Claim,Employee,User_Profile
 from django.contrib.auth import logout
-from django.contrib.auth import logout
+
 
 # Create your views here.
 
